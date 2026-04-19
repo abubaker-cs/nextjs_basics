eaa65f6ae81d660f614c20b6afa0c4aa94a88d9d
Mohammad Abubaker <Mohammad_Abubaker@yahoo.com>
2026-04-20 02:50:53 +0500

    Added React Complier to the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Hello from "@/app/components/hello";
 
 const Home = () => {
 
     console.log('Recorded: Home page');
 
     return (
-        <div className="text-5xl underline">Home 101</div>
+        <main>
+            <div className="text-5xl underline">Home 101 2</div>
+            <Hello />
+        </main>
     )
 }
 export default Home
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  reactCompiler: true,
   allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', '192.168.2.100'],
   experimental:{
       turbopackFileSystemCacheForDev: true,
